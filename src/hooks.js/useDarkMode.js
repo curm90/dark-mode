0e7ac8dc0080dde9436a6eeb847a7a59f6cd2c59
Liam Sutton <devliam90@gmail.com>
2019-10-02 12:41:40 +0100

    wire up hooks and watch the magic happen

--- src/hooks.js/useDarkMode.js
+++ src/hooks.js/useDarkMode.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useDarkMode = () => {
+const useDarkMode = (key, initialValue) => {
   const [isDarkMode, setIsDarkMode] = useLocalStorage(key, initialValue);
 
   useEffect(() => {
+    const bodyEl = document.querySelector('body');
     if (isDarkMode) {
-      const bodyEl = document.querySelector('body');
       bodyEl.classList.add('dark-mode');
     } else {
       bodyEl.classList.remove('dark-mode');
--- src/hooks.js/useLocalStorage.js
+++ src/hooks.js/useLocalStorage.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     const localStorageValue = localStorage.getItem(key);
